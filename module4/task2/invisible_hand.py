394c4a0c616081ce4fa5821ad90ddca103812ffd
Darwin Do <darwin78913@gmail.com>
2023-11-08 12:14:23 -0500

    wip

--- module4/task2/invisible_hand.py
+++ module4/task2/invisible_hand.py
@@ -1,8 +1,15 @@
 import pygame
 import socket
-import select
+
+from typing import List
+from perlin_noise import PerlinNoise
 
 FULLSCREEN = False
+
+# Wireless initialization
+ESP32_ADDR = ("192.168.0.137", 6101)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket.connect(ESP32_ADDR)
 
 pygame.init()
 
@@ -18,14 +25,10 @@ else:
 screen = pygame.display.set_mode((WIDTH, HEIGHT), screen_flags)
 pygame.display.set_caption("The Invisible Hand")
 
-
 clock = pygame.time.Clock()
 running = True
 
-# Wireless initialization
-ESP32_ADDR = ("192.168.0.137", 6101)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client_socket.connect(ESP32_ADDR)
+faucet_img = pygame.transform.scale_by(pygame.image.load("resources/faucet.png"), 0.25)
 
 class Button:
     def __init__(self, x, y, width, height, text, callback, color=(1, 129, 129), text_color=(255, 255, 255), alt_text=""):
@@ -59,7 +62,7 @@ class TextBox(Button):
         self._cursor_visible = True
 
     def add_to_textbox(self, new_text: str):
-        self.text += new_text
+        self.text += new_text.upper()
 
     def backspace(self):
         if len(self.text) > 0:
@@ -80,14 +83,25 @@ class TextBox(Button):
         print("text box clicked")
 
 class Money:
-    FALL_RATE = -0.005
+    WIDTH = 64
+    HEIGHT = 64
+    IMG = pygame.transform.scale(pygame.image.load("resources/money.png"), (WIDTH, HEIGHT))
 
     def __init__(self, x, y):
         self._x = x
         self._y = y
+        self._fall_rate = 1
         
     def update_money(self):
-        self._y = round(self._y + Money.FALL_RATE)
+        self._y += self._fall_rate
+        self._fall_rate += 0.01
+
+    def draw(self):
+        self.update_money()
+        screen.blit(Money.IMG, (self._x, self._y))
+
+    def out_of_bounds(self):
+        return self._y - 64 > HEIGHT
 
 def draw_text(text, font, color, x, y):
     text_surface = font.render(text, True, color)
@@ -104,13 +118,24 @@ description_text = ""
 def fund_cfpb():
     pass
 
+def update_stock(new_ticker: str):
+    global description_text
+    description_text = f"Current Stock: {new_ticker}"
+
+
 
 cfpb_button = Button((WIDTH*5//6)- 300, HEIGHT*5//6, 300, 50, "Fund CFPB", fund_cfpb, alt_text="Start recording new soundscape with junglebox")
 title_text = "The Invisible Hand"
-stock_input = TextBox(30, 30, 300, 36)
+title_text_emoji = "🤑"
+stock_input = TextBox((WIDTH//2) - 150, HEIGHT*5//6, 300, 36)
 
-clickable_elements = [cfpb_button, stock_input]
-money_list = []
+clickable_elements = [stock_input]
+money_list: List[Money] = []
+
+noise = PerlinNoise(10.23)
+
+# initialize program to cheesecake factory
+update_stock("CAKE")
 
 print("Starting pygame interface")
 while running:
@@ -129,6 +154,8 @@ while running:
                 
                 if background_clicked:
                     print(event)
+                    money_list.append(Money(event.pos[0]-Money.WIDTH//2, event.pos[1]-Money.HEIGHT//2))
+            
         elif event.type == pygame.MOUSEMOTION:
             is_hovered = False
             for elem in clickable_elements:
@@ -145,25 +172,45 @@ while running:
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
-                pass
+                update_stock(stock_input.text)
             elif event.key == pygame.K_BACKSPACE:
                 stock_input.backspace()
             else:
-                stock_input.add_to_textbox(event.unicode)
+                cap = event.unicode.upper()
+                if ord(cap) >= ord('A') and ord(cap) <= ord('Z'):
+                    print(event)
+                    stock_input.add_to_textbox(event.unicode)
 
     screen.fill((1, 129, 129))
     # screen.blit(bg_img, (0, 0))
-
+    
     # Define font and text content
-    font = pygame.font.SysFont("SourceCodePro-Black.otf", 16)
-
+    font = pygame.font.SysFont("SourceCodePro-Black.otf", 72)
+    
     # Draw the text element on the screen
-    title_text_pos = get_text_offset((WIDTH//2, HEIGHT*2//5), title_text, font)
+    title_text_pos = get_text_offset((WIDTH//2, HEIGHT*1//5), title_text, font)
     description_text_pos = get_text_offset((WIDTH//2, HEIGHT*4//6), description_text, font)
     draw_text(title_text, font, (223, 223, 96), title_text_pos[0], title_text_pos[1])
     draw_text(description_text, font, (192, 192, 192), description_text_pos[0], description_text_pos[1])
+    
+    # draw UI elements
     cfpb_button.draw()
     stock_input.draw()
+
+    # draw money animations
+    for elem in money_list:
+        if not elem.out_of_bounds():
+            elem.draw()
+        else:
+            money_list.remove(elem)
+
+    # draw faucet
+    faucet_box = screen.blit(faucet_img, (-30, 30))
+
+    # trickle down economics
+    if abs(noise(pygame.time.get_ticks()/10000)) < 0.01:
+        money_list.append(Money(faucet_box.bottomright[0]-40, faucet_box.bottomright[1]-15))
+        # print(abs(noise(pygame.time.get_ticks()/10000)))
 
     pygame.display.flip()
 
