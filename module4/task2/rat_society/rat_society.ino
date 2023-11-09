16a9eb00548412ca84ecbe5a75b95e240e8627aa
Darwin Do <darwin78913@gmail.com>
2023-11-08 22:30:19 -0500

    edit code

--- module4/task2/rat_society/rat_society.ino
+++ module4/task2/rat_society/rat_society.ino
@@ -14,13 +14,21 @@
 #define STOCK_API_FORMAT "https://api.polygon.io/v2/aggs/ticker/%s/prev?apiKey=%s"
 #define TEST_JSON "{\"ticker\":\"AAPL\",\"queryCount\":1,\"resultsCount\":1,\"adjusted\":true,\"results\":[{\"T\":\"AAPL\",\"v\":7.9829246e+07,\"vw\":175.5751,\"o\":174.24,\"c\":176.65,\"h\":176.82,\"l\":173.35,\"t\":1699041600000,\"n\":858038}],\"status\":\"OK\",\"request_id\":\"509cb86f6d68ea6eff37a002f144438b\",\"count\":1}"
 
-// Motor/Servo Definitions
+// Hardware Definitions
 #define IN1 19
 #define IN2 18
 #define IN3 5
 #define IN4 17
-#define SERVO 26
 #define stepsPerRevolution 2048
+
+/*
+WARNING, CANNOT USE ADC PINS:
+GPIO4, GPIO0, GPIO2, GPIO15, GPIO13, GPIO12, GPIO14, GPIO27, GPIO25 and GPIO26
+WHEN WIFI IS ENABLED!!!
+*/
+#define PHOTO 34
+#define SERVO 26
+
 
 
 Stepper spinner(stepsPerRevolution, IN1, IN3, IN2, IN4);
@@ -42,6 +50,8 @@ void setup(){
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, WIFI_PASS);
 
+  pinMode(PHOTO, INPUT);
+
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
@@ -50,12 +60,10 @@ void setup(){
   Serial.println(WiFi.localIP());
 
   server.begin();
-  Serial.println(getStockChange("AAPL"));
+  // Serial.println(getStockChange("AAPL"));
 }
 
 void loop(){
-  unsigned long start = millis();
-  spinner.setSpeed(spinnerSpeed);
   WiFiClient client = server.available();
   
   if (client) {
@@ -77,9 +85,20 @@ void loop(){
         }
       }
 
-
-      // main loop here
+      actuatorControl();
     }
+  } else {
+    actuatorControl();
+  }
+
+}
+
+void actuatorControl() {
+  Serial.println(analogRead(PHOTO));
+  int light = analogRead(PHOTO);
+  if (light > 100) {
+    spinner.setSpeed(map(light, 100, 4095, 4, 12));
+    spinner.step(64);
   }
 }
 
@@ -87,11 +106,11 @@ float getStockChange(const char* tickerVal) {
   HTTPClient http;
   sprintf(apiEndpoint, STOCK_API_FORMAT, tickerVal, API_KEY);
   http.begin(apiEndpoint);
-  int requestStatus = http.GET();
-  // int requestStatus = 5;
+  // int requestStatus = http.GET();
+  int requestStatus = 5;
   if (requestStatus > 0) {
-    String payload = http.getString();
-    // String payload = TEST_JSON;
+    // String payload = http.getString();
+    String payload = TEST_JSON;
     Serial.println(payload);
     DeserializationError err = deserializeJson(responseJson, payload);
     if (err) {
